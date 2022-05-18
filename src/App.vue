e31ee63f721e4cdc3f500de02cff595f34f8e960
L1brA7 <umashev58@gmail.com>
2022-05-18 19:53:07 +0300

    сделана основная часть вёрстки

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,223 @@
 <template>
   <div class="app-container">
-    <div></div>
+    <div class="main-container">
+      <div class="top-bar">
+        <div class="logo"></div>
+        <GenericButton/>
+      </div>
+
+      <div class="section">
+        <div class="section__top-part">
+          <div class="section__number number" style="margin-top: 28px">01</div>
+          <div class="section__title main-title">Фабрика по производству биороботов</div>
+          <div class="robots"></div>
+        </div>
+        <div class="section__main-part">
+          <div class="section__scroll">
+            <div class="scroll">скролл</div>
+            <div class="scroll-arrow"></div>
+          </div>
+          <div class="section__undertitle undertitle">класса «монитор-кресло»</div>
+        </div>
+      </div>
+
+      <div class="section">
+        <div class="section__top-part">
+          <div class="section__number number">02</div>
+          <div class="section__title title">Кошелёк криптовалют</div>
+        </div>
+        <div class="section__main-part">
+          <div class="section__scroll"></div>
+          <div class="section__component">
+            <CryptoWallet/>
+          </div>
+        </div>
+      </div>
+
+      <div class="section">
+        <div class="section__top-part">
+          <div class="section__number number">03</div>
+          <div class="section__title title">Рынок комплектующих</div>
+        </div>
+        <div class="section__main-part">
+          <div class="section__scroll"></div>
+          <div class="section__component">
+            <PartMarket/>
+          </div>
+        </div>
+      </div>
+
+      <div class="section">
+        <div class="section__top-part">
+          <div class="section__number number">04</div>
+          <div class="section__title title">Склад</div>
+        </div>
+        <div class="section__main-part">
+          <div class="section__scroll"></div>
+          <div class="section__component">
+            <PartStorage/>
+          </div>
+        </div>
+      </div>
+
+      <div class="section">
+        <div class="section__top-part">
+          <div class="section__number number">05</div>
+          <div class="section__title title">Производство</div>
+        </div>
+        <div class="section__main-part">
+          <div class="section__scroll"></div>
+          <div class="section__component">
+            <PartStorage/>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
+import GenericButton from './components/GenericButton.vue';
+import CryptoWallet from './components/CryptoWallet.vue';
+
 export default {
   name: 'App',
+  components: {
+    GenericButton,
+    CryptoWallet,
+  },
   data() {
     return {
       coins: 0,
+      h_level: 1,
       biobots: [
         {
-          id: 0, hand: true, microchip: true, soul: true, gender: 'male',
+          id: 0, hand: true, microchip: true, soul: true, gender: 'male', type: 'frontend',
         },
       ],
     };
   },
 };
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
   .app-container {
-    position: fixed;
+    position: absolute;
     top: 0;
     left: 0;
     width: 100%;
-    height: 100%;
     background: radial-gradient(82.33% 70.44% at 50% 0%, #26394D 0%, #212529 100%), #FFFFFF;
+  }
+  .top-bar {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-bottom: 120px;
+  }
+  .logo {
+    width: 78px;
+    height: 48px;
+    float: left;
+    background: url('assets/pacman-logo.svg');
+  }
+  .main-container {
+    margin: 48px auto;
+    width: 1016px;
+  }
+  .section {
+    display: flex;
+    flex-direction: column;
+    width: 1016px;
+    &__top-part {
+      display: flex;
+    }
+    &__main-part {
+      margin-bottom: 80px;
+      display: flex;
+    }
+    &__number {
+      width: 24px;
+      height: 24px;
+      text-align: center;
+      margin-top: 8px;
+    }
+    &__title {
+      margin-left: 106px;
+    }
+    &__scroll {
+      width: 24px;
+      height: 100px;
+    }
+    &__component {
+      margin-top: 60px;
+      margin-left: 106px;
+    }
+    &__undertitle {
+      margin-top: 24px;
+      margin-left: 106px;
+    }
+  }
+  .main-title {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 600;
+    letter-spacing: 0.015em;
+    font-size: 56px;
+    line-height: 72px;
+    color: #FFFFFF;
+    width: 470px;
+  }
+  .undertitle {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 600;
+    letter-spacing: 0.022em;
+    font-size: 24px;
+    line-height: 32px;
+    color: #A3B8CC;
+  }
+  .number {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 24px;
+    color: #667380;
+  }
+  .title {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 600;
+    letter-spacing: 0.02em;
+    font-size: 32px;
+    line-height: 40px;
+    color: #FFFFFF;
+  }
+  .scroll {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 24px;
+    display: flex;
+    align-items: center;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+    color: #667380;
+    transform: rotate(-90deg);
+  }
+  .scroll-arrow {
+    width: 24px;
+    height: 24px;
+    margin-top: 13px;
+    margin-left: 6px;
+    background: url('./assets/scroll-arrow.svg');
+    background-repeat: no-repeat;
+  }
+  .robots {
+    width: 366px;
+    height: 320px;
+    margin-left: 662px;
+    background-repeat: no-repeat;
   }
 </style>
---
+++ src/components/CryptoWallet.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="coins">
+    <div class="coins__coin" v-for="coin in balance" :key="coin"></div>
+  </div>
+  <div class="balance balance_text">{{ balance }} biorobo {{ text_cased }}</div>
+  <div class="make-money-button make-money-button_text">Нацыганить</div>
+  <div class="condition-checkbox"></div>
+  <div class="condition condition_text">Цыганить по 5 монет</div>
+</template>
+
+<script>
+export default {
+  name: 'CryptoWallet',
+  data() {
+    return {
+      balance: 45,
+      text_cased: 'монет',
+    };
+  },
+  mounted() {
+    if (this.balance >= 11 && this.balance <= 19) {
+      this.text_cased = 'монет';
+    } else if (this.balance % 10 >= 2 && this.balance % 10 <= 4) {
+      this.text_cased = 'монеты';
+    } else if (this.balance % 10 === 1) {
+      this.text_cased = 'монета';
+    }
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .coins {
+    display: flex;
+    margin-left: 247px;
+    width: 100%;
+    &__coin {
+      width: 20px;
+      height: 20px;
+      margin-right: -25.6px;
+      background: url('../assets/coin.svg');
+      background-repeat: no-repeat;
+    }
+  }
+  .balance {
+    margin-top: 24px;
+  }
+  .balance_text {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 700;
+    letter-spacing: 0.01em;
+    font-size: 24px;
+    line-height: 24px;
+    color: #A3B8CC;
+  }
+  .make-money-button {
+    margin-top: 44px;
+  }
+  .make-money-button_text {
+    display: inline-block;
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 500;
+    letter-spacing: 0.035em;
+    font-size: 16px;
+    line-height: 24px;
+    color: #FF7F22;
+    text-decoration: 1px underline #ff7e229d;
+    text-underline-position: under
+  }
+  .condition-checkbox {
+    display: inline-block;
+    width: 24px;
+    height: 24px;
+    margin-bottom: -8px;
+    margin-left: 29px;
+    border: 2px solid #A3B8CC;
+    box-sizing: border-box;
+  }
+  .condition {
+    display: inline-block;
+    margin-left: 12px;
+  }
+  .condition_text {
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+    color: #FFFFFF;
+  }
+</style>
