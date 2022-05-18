2cb8995fb278a43adf2527be0eaf413994036a22
L1brA7 <umashev58@gmail.com>
2022-05-18 13:44:49 +0300

    background done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="container"></div>
+  <div class="app-container">
+    <div></div>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
+  data() {
+    return {
+      coins: 0,
+      biobots: [
+        {
+          id: 0, hand: true, microchip: true, soul: true, gender: 'male',
+        },
+      ],
+    };
+  },
 };
 </script>
-
-<style lang="sass">
-  .container
-    width: 100%
+<style lang="scss">
+  .app-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: radial-gradient(82.33% 70.44% at 50% 0%, #26394D 0%, #212529 100%), #FFFFFF;
+  }
 </style>
