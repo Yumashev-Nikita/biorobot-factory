312fbd3a67323b43dc3f68bb6a26e95ee67974b3
L1brA7 <umashev58@gmail.com>
2022-05-28 13:57:50 +0300

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vuejs-accessibility/click-events-have-key-events': 'off',
+    'linebreak-style': ['error', (process.platform === 'win32' ? 'windows' : 'unix')],
   },
 };
